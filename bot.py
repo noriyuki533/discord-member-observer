31abc1a43f8804d1aa585c4df219959e79785fbb
noriyuki <ka01dftn@gmail.com>
2020-07-29 00:24:43 +0900

    AutoFormat適用

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ from discord.ext import commands
 import traceback
 import datetime
 
-TOKEN = 'NzM1ODIyNjYzMTY3MTE1MzI1.Xxl6Bg.fbaXzdJY7TIRFtZbFNC0cNwip1U'
+TOKEN = "NzM1ODIyNjYzMTY3MTE1MzI1.Xxl6Bg.fbaXzdJY7TIRFtZbFNC0cNwip1U"
+
 
 class MemberObserver(commands.Bot):
     def __init__(self, command_prefix):
@@ -15,26 +16,34 @@ class MemberObserver(commands.Bot):
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
         dt_now = datetime.datetime.now()
-        dt_now_str = dt_now.strftime('%Y-%m-%d %H:%M:%S')
+        dt_now_str = dt_now.strftime("%Y-%m-%d %H:%M:%S")
         member_avatar_url = str(after.avatar_url)
 
         if not before.status == after.status:
             update_str = "**{}** -> **{}**".format(before.status, after.status)
             msg = """
             **{0.name}**が**{0.status}**になりました.
-            """.format(after)
+            """.format(
+                after
+            )
             footer_str = "ステータスの変更"
         elif not before.activity == after.activity:
-            before_act_str = before.activity.name if before.activity else before.activity
+            before_act_str = (
+                before.activity.name if before.activity else before.activity
+            )
             after_act_str = after.activity.name if after.activity else after.activity
             if not after.activity:
                 msg = """
                 **{0.name}**が**{1}**を終了しました.
-                """.format(before, before_act_str)
+                """.format(
+                    before, before_act_str
+                )
             else:
                 msg = """
                 **{0.name}**が**{1}**を開始しました.
-                """.format(after, after_act_str)
+                """.format(
+                    after, after_act_str
+                )
             update_str = "**{}** -> **{}**".format(before_act_str, after_act_str)
             footer_str = "ゲームアクティビティの変更"
         else:
@@ -48,12 +57,11 @@ class MemberObserver(commands.Bot):
 
         not_channel = self.get_channel(735828665438306365)
         await not_channel.send(embed=msg_embed)
-                
-    
+
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         dt_now = datetime.datetime.now()
-        dt_now_str = dt_now.strftime('%Y-%m-%d %H:%M:%S')
+        dt_now_str = dt_now.strftime("%Y-%m-%d %H:%M:%S")
         member_avatar_url = str(member.avatar_url)
         footer_str = "ボイスステータスの変更"
 
@@ -62,25 +70,39 @@ class MemberObserver(commands.Bot):
             if not before.channel:
                 msg = """
                 **{0}**が**{1}**に参加しました.
-                """.format(member, after.channel.name)
+                """.format(
+                    member, after.channel.name
+                )
             elif not after.channel:
                 msg = """
                 **{0}**が**{1}**から退出しました.
-                """.format(member, before.channel.name)
+                """.format(
+                    member, before.channel.name
+                )
             else:
                 msg = """
                 **{0}**が**{1}**から**{2}**に移動しました.
-                """.format(member, before.channel.name, after.channel.name)
+                """.format(
+                    member, before.channel.name, after.channel.name
+                )
         elif not before.self_deaf == after.self_deaf:
-            update_str = "deaf: **{}** -> **{}**".format(before.self_deaf, after.self_deaf)
+            update_str = "deaf: **{}** -> **{}**".format(
+                before.self_deaf, after.self_deaf
+            )
             msg = """
             **{0}**が**スピーカーミュート**{1}.
-            """.format(member, "しました" if after.self_deaf else "を解除しました")
+            """.format(
+                member, "しました" if after.self_deaf else "を解除しました"
+            )
         elif not before.self_mute == after.self_mute:
-            update_str = "mute: **{}** -> **{}**".format(before.self_mute, after.self_mute)
+            update_str = "mute: **{}** -> **{}**".format(
+                before.self_mute, after.self_mute
+            )
             msg = """
             **{0}**が**ミュート**{1}.
-            """.format(member, "しました" if after.self_mute else "を解除しました")
+            """.format(
+                member, "しました" if after.self_mute else "を解除しました"
+            )
         else:
             return
 
@@ -92,9 +114,12 @@ class MemberObserver(commands.Bot):
 
         not_channel = self.get_channel(735894701139296277)
         await not_channel.send(embed=msg_embed)
+
+
 def main():
-    mo = MemberObserver(command_prefix='!')
+    mo = MemberObserver(command_prefix="!")
     mo.run(TOKEN)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
